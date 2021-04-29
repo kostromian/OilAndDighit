ae3e3f9c7b7ce07d5afd5ca89940bfb03d578f97
Alex <kostromian@gmail.com>
2021-04-29 15:57:39 +0300

    struct add

---
+++ Company.py
@@ -0,0 +1,8 @@
+atribute_names = []
+
+class Company():
+    def __init__(self, name, mother):
+        self.name = name
+        self.atributes = []
+        self.mother = mother
+        self.children = []
